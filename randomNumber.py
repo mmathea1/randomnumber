b8a19b9be48701b4cd606a7be2be47e4d2e18f6a
MishyMinty <mustbemishtaken@gmail.com>
2017-09-24 00:05:07 +0300

    modified a line

--- randomNumber.py
+++ randomNumber.py
@@ -1,13 +1,17 @@
 from random import randint
 
-random = randint(0,100)
-print random
+random = randint(1,100)
 guess = int(input("Guess a number between 0 and 100: "))
+    
 if guess < random:
-    print 'Sorry but ' +guess+ 'is lower than the number'
+    print 'Sorry but %d is LESSER than the number' % guess
+        
 elif guess > random:
-    print 'Sorry but ' +guess+ 'is greater than the number'
+    print 'Sorry but %d is GREATER than the number' % guess
+        
 elif guess == random:
-    print 'Yes! You got it! Your guess, ' +guess+ ' is ' + random
+    print 'Yes! You got it! Your guess, %d is %d ' %guess %random
+       
 else:
     print 'I didnt catch that, try again please'
+ 
