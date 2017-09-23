90bb7b829370b6c87b48db7a59209693fa70cc46
MishyMinty <mustbemishtaken@gmail.com>
2017-09-24 01:30:04 +0300

    about to calculate days to birthday

--- birthdayapp.py
+++ birthdayapp.py
@@ -6,7 +6,10 @@ bday = input("What day were you born? [DD]: ")
 
 dt = datetime(year = byear, month = bmonth, day = bday)
 birthday = datetime.strftime(dt, '%d/%m/%Y')
+#now = datetime(year = now.year, month = now.month, day = now.day)
+#diff = now-dt
+#datetime.timedelta(7)
+#print diff.days
 print "It looks like you were born on %s" %birthday
-#get input
-#concactenate
-#format
+
+
